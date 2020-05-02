be82079640959cd1b7185ed0d16b5a25ccc71837
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-05-02 06:19:00 +0200

    handle external comments on issues reported by us

--- run.go
+++ run.go
@@ -308,10 +308,29 @@ func doIssuesTriage(issues []Issue) ([]Issue, []Issue, []Issue) {
 	notAnsweredIssues := []Issue{}
 	for i := 0; i < len(issues); i++ {
 		issue := issues[i]
+		comments := issue.Comments.Edges
 		if issue.AuthorAssociation == "MEMBER" {
-			ourIssues = append(ourIssues, issue)
+			j := len(comments) - 1
+			lastAuthorAssociation := ""
+			for ; j >= 0; j-- {
+				comment := comments[j].Node
+				if comment.Author.Login != "issuehunt-app" && lastAuthorAssociation == "" {
+					lastAuthorAssociation = comment.AuthorAssociation
+				}
+				if comment.Author.Login != "issuehunt-app" && comment.AuthorAssociation != "MEMBER" {
+					break
+				}
+			}
+			if j == -1 {
+				ourIssues = append(ourIssues, issue)
+			} else {
+				if lastAuthorAssociation == "MEMBER" {
+					answeredIssues = append(answeredIssues, issue)
+				} else {
+					notAnsweredIssues = append(notAnsweredIssues, issue)
+				}
+			}
 		} else {
-			comments := issue.Comments.Edges
 			if len(comments) == 0 {
 				notAnsweredIssues = append(notAnsweredIssues, issue)
 			} else {
