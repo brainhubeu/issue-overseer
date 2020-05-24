34ca3583178065b957c49a4b2094ae2e0a7cf3dc
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-05-24 04:54:16 +0200

    test not creating already created labels

--- GithubOperator/github_operations_test.go
+++ GithubOperator/github_operations_test.go
@@ -104,4 +104,60 @@ func TestGithubOperator(t *testing.T) {
 			[]interface{}{"repo-3", Interfaces.Label{Name: "label-3", Color: "color-3"}},
 		})
 	})
+
+	t.Run("does not create already created labels", func(t *testing.T) {
+		mockCreateLabelsParams := []interface{}{}
+		repoNames := []string{
+			"repo-1",
+			"repo-2",
+			"repo-3",
+		}
+		answeringLabels := []Interfaces.Label{
+			Interfaces.Label{Name: "label-1", Color: "color-1"},
+			Interfaces.Label{Name: "label-2", Color: "color-2"},
+			Interfaces.Label{Name: "label-3", Color: "color-3"},
+		}
+
+		mockTriageManyIssues = func(issues []Interfaces.Issue) ([]Interfaces.Issue, []Interfaces.Issue, []Interfaces.Issue) {
+			return []Interfaces.Issue{}, []Interfaces.Issue{}, []Interfaces.Issue{}
+		}
+		mockFindLabels = func(repoName string) []Interfaces.Label {
+			if repoName == "repo-1" {
+				return []Interfaces.Label{
+					Interfaces.Label{Name: "label-1", Color: "color-1"},
+					Interfaces.Label{Name: "label-2", Color: "color-2"},
+				}
+			}
+			if repoName == "repo-2" {
+				return []Interfaces.Label{
+					Interfaces.Label{Name: "label-1", Color: "color-1"},
+				}
+			}
+			if repoName == "repo-3" {
+				return []Interfaces.Label{
+					Interfaces.Label{Name: "label-1", Color: "color-1"},
+					Interfaces.Label{Name: "label-2", Color: "color-2"},
+					Interfaces.Label{Name: "label-3", Color: "color-3"},
+				}
+			}
+			return []Interfaces.Label{}
+		}
+		mockCreateLabel = func(repoName string, label Interfaces.Label) {
+			mockCreateLabelsParams = append(mockCreateLabelsParams, []interface{}{repoName, label})
+		}
+		mockFindIssues = func(repoName string) []Interfaces.Issue {
+			return []Interfaces.Issue{}
+		}
+
+		githubClient := MockGithubClient{}
+		issuesTriage := MockIssuesTriage{}
+		githubOperator := InitGithubOperator(githubClient, issuesTriage, answeringLabels, "label-1", "label-2", "label-3")
+		githubOperator.UpdateRepos(repoNames)
+
+		assert.Equal(t, mockCreateLabelsParams, []interface{}{
+			[]interface{}{"repo-1", Interfaces.Label{Name: "label-3", Color: "color-3"}},
+			[]interface{}{"repo-2", Interfaces.Label{Name: "label-2", Color: "color-2"}},
+			[]interface{}{"repo-2", Interfaces.Label{Name: "label-3", Color: "color-3"}},
+		})
+	})
 }
