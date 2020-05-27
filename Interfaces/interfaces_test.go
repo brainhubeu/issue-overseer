4d403d21c9328bb1b55a5e48858cd2cb3d3c51ce
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-05-27 06:01:03 +0200

    empty Interfaces test suite

---
+++ Interfaces/interfaces_test.go
@@ -0,0 +1,17 @@
+package Interfaces
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestInterfaces(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Interfaces")
+}
+
+/*
+ * no tests because no statements;
+ * this file is in order to measure test coverage
+ */
